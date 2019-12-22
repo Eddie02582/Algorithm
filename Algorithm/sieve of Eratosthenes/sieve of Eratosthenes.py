4e98d6e930e7694a1651274b4c4c61dda104d55b
Eddie02582 <38853252+Eddie02582@users.noreply.github.com>
2019-12-22 15:22:49 +0800

    Update

--- Algorithm/sieve of Eratosthenes/sieve of Eratosthenes.py
+++ Algorithm/sieve of Eratosthenes/sieve of Eratosthenes.py
@@ -1,9 +1,12 @@
 def Primes(n) :
-    IsPrime = 0, [True] * n
-    for i in range(2, n):
+    IsPrime = [True] * (n + 1)
+    IsPrime[0] = False
+    IsPrime[1] = False
+    primes = []
+    for i in range(2, n + 1):
         if IsPrime[i]:            
-            for j in range(i*i, n, i):
+            for j in range(i * i, n + 1, i):
                 IsPrime[j] = False
-    return [x for x in range(2, n + 1) if IsPrime[x]]
+            primes.append(i)
+    return primes
 
-    
