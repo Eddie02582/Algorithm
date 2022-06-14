d58cb25cf97d7f5398cecbb50de94db0bb7a2baf
Eddie02582 <38853252+Eddie02582@users.noreply.github.com>
2022-06-14 16:00:54 +0800

    update Merge

--- Sort/Merge Sort/MergeSort.cpp
+++ Sort/Merge Sort/MergeSort.cpp
@@ -1,63 +1,3 @@
-# Merge Sort 
-
-## Algorithm 
-將兩個有序對數組合併成一個更大的有序數組
-
-
-動畫可以參考<a href ="https://visualgo.net/en/sorting">visualgo </a>
-
-步驟
-<ul>
-    <li>將陣列拆程左邊和右邊</li>
-    <li>左邊呼叫mergeSort</li>
-    <li>右邊呼叫mergeSort</li>
-    <li>合併</li>
-</ul>
-
-圖片取自於geeksforgeeks
-<img src = "https://www.geeksforgeeks.org/wp-content/uploads/Merge-Sort-Tutorial.png"></img>
-
-
-## Example 
-
-
-## Python
-
-
-``` python
-def mergeSort(arr):          
-    if len(arr) <= 1:
-        return arr
-    mid = len(arr) // 2
-    left = arr[:mid]
-    right = arr[mid:]   
-    mergeSort(left)    
-    mergeSort(right)       
-
-    i,j =0,0
-    while i <len(left) and j <len(right):
-        if left[i] < right[j]:
-            arr[i + j] = left[i]
-            i += 1
-        else:
-            arr[i + j] = right[j]
-            j += 1
-        
-    while i < len(left): 
-        arr[i + j] = left[i]
-        i+= 1      
-      
-    while j < len(right): 
-        arr[i + j] = right[j]
-        j+= 1
-       
-        
-    return arr  
-
-```
-
-## C++
-```c++
 #include <string>
 #include <iostream>
 
@@ -121,11 +61,3 @@ void merge(int arr[], int front, int mid, int end) {
         arr[k++] = R[j++];  
     } 
 }
-```
-
-
-
-
-
-
-
